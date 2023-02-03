900e947b6f1a45f2425af56068fbbb2e481310a9
Roman Pediash <peaktib@gmail.com>
2023-02-03 23:44:33 +0200

    Task 2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,14 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+const listIngredients = document.querySelector('#ingredients');
+
+const listElArr = [];
+
+for (const element of ingredients) {
+  const listEl = document.createElement('li');
+  listEl.textContent = element;
+  listEl.classList.add('item');
+  listElArr.push(listEl);
+}
+listIngredients.append(...listElArr);
