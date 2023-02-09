a683f06696d31f6544f92f1c0dcf88d301a0617a
Roman Pediash <peaktib@gmail.com>
2023-02-09 23:48:31 +0200

    Task8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,23 @@
+const loginForm = document.querySelector(".login-form");
 
+loginForm.addEventListener('submit', onFormSubmit);
+
+const objInputForm = {};
+
+function onFormSubmit(event) {
+    event.preventDefault();
+  
+    const email = event.currentTarget.elements.email.value;
+    const pass = event.currentTarget.elements.password.value;
+    const formData = {
+        email,
+        pass,
+    }
+    
+    if (email === "" || pass === "") {
+        alert("All fields is required!")
+    }
+
+    console.log(formData);
+    event.currentTarget.reset();
+}
