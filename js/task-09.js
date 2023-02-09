b8fad7a86af7fd28c1dccbb487c28966dfd96ab9
Roman Pediash <peaktib@gmail.com>
2023-02-10 00:04:02 +0200

    Task9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const btnChangeColor = document.querySelector(".change-color");
+const nameColorText = document.querySelector(".color");
+const body = document.querySelector("body");
+
+btnChangeColor.addEventListener('click', onBtnChangeColor);
+
+function onBtnChangeColor() {
+  body.style.backgroundColor = getRandomHexColor();
+  nameColorText.textContent = body.style.backgroundColor;
+}
